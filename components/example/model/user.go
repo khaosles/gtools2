7f2f4a80f6d6ab13897eb323223b82d5157bb54a
khaosles <khaosles@163.com>
2023-06-15 00:10:06 +0800

    1

---
+++ components/example/model/user.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/khaosles/gtools2/components/g"
+)
+
+/*
+   @File: user.go
+   @Author: khaosles
+   @Time: 2023/6/12 10:56
+   @Desc:
+*/
+
+type User struct {
+	g.Model
+	UserName string
+}
+
+func (User) TableName() string {
+	return "user"
+}
