31c10cbf5c5c5a58d9f9e45f61bba2e8acee9735
khaosles <khaosles@163.com>
2023-06-17 21:01:16 +0800

    1

--- cmd/tmpl.go
+++ cmd/tmpl.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"os"
-	"text/template"
-	"time"
-
-	"github.com/iancoleman/strcase"
+	"github.com/khaosles/gtools2/components/tmpl"
 	gpath "github.com/khaosles/gtools2/utils/path"
 )
 
@@ -18,49 +14,23 @@ import (
 
 func main() {
 	project := "data-search/app"
-	modelName := "metaData"
 	root := gpath.Join(gpath.RootPath(), "app")
-	for _, name := range []string{"service.go.internal.tmpl", "service.go.tmpl", "mapper.go.internal.tmpl", "mapper.go.tmpl", "model.go.tmpl"} {
-		filepath := gpath.Join(gpath.RootPath(), "resource", "template", name)
-		tmpl, err := template.ParseFiles(filepath)
-		if err != nil {
-			panic(err)
-		}
+	models := []string{
+		"sys_config",
+		"sys_config_data",
+		"log_login",
+		"log_operator",
+		"sys_dictionary",
+		"sys_dictionary_data",
+		"sys_user",
+		"sys_role",
+		"sys_menu",
+		"sys_user_role",
+		"sys_role_menu",
+	}
 
-		data := struct {
-			Name      string
-			NameUpper string
-			NameSnake string
-			Date      string
-			Project   string
-		}{
-			Name:      strcase.ToLowerCamel(modelName),
-			NameUpper: strcase.ToCamel(modelName),
-			NameSnake: strcase.ToSnake(modelName),
-			Date:      time.Now().Format(time.DateTime),
-			Project:   project,
-		}
-		var path string
-		switch name {
-		case "service.go.internal.tmpl":
-			path = gpath.Join(root, "/service/internal/"+strcase.ToSnake(modelName)+"_service_impl.go")
-		case "service.go.tmpl":
-			path = gpath.Join(root, "/service/"+strcase.ToSnake(modelName)+"_service.go")
-		case "mapper.go.internal.tmpl":
-			path = gpath.Join(root, "/mapper/internal/"+strcase.ToSnake(modelName)+"_mapper_impl.go")
-		case "mapper.go.tmpl":
-			path = gpath.Join(root, "/mapper/"+strcase.ToSnake(modelName)+"_mapper.go")
-		case "model.go.tmpl":
-			path = gpath.Join(root, "/model/"+strcase.ToSnake(modelName)+".go")
-		}
-
-		gpath.MkParentDir(path)
-		fp, _ := os.Create(path)
-
-		err = tmpl.Execute(fp, data)
-		if err != nil {
-			panic(err)
-		}
+	for _, model := range models {
+		tmpl.Run(project, model, root)
 	}
 
 }
---
+++ components/tmpl/tmpl.go
@@ -0,0 +1,62 @@
+package tmpl
+
+import (
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/iancoleman/strcase"
+	gpath "github.com/khaosles/gtools2/utils/path"
+)
+
+/*
+   @File: main.go
+   @Author: khaosles
+   @Time: 2023/6/13 21:17
+   @Desc:
+*/
+
+func Run(project, modelName, root string) {
+	for _, name := range []string{"service.go.internal.tmpl", "service.go.tmpl", "mapper.go.internal.tmpl", "mapper.go.tmpl", "model.go.tmpl"} {
+		filepath := gpath.Join(gpath.RootPath(), "resource", "template", name)
+		tmpl, err := template.ParseFiles(filepath)
+		if err != nil {
+			panic(err)
+		}
+		data := struct {
+			Name      string
+			NameUpper string
+			NameSnake string
+			Date      string
+			Project   string
+		}{
+			Name:      strcase.ToLowerCamel(modelName),
+			NameUpper: strcase.ToCamel(modelName),
+			NameSnake: strcase.ToSnake(modelName),
+			Date:      time.Now().Format(time.DateTime),
+			Project:   project,
+		}
+		var path string
+		switch name {
+		case "service.go.internal.tmpl":
+			path = gpath.Join(root, "/service/internal/"+strcase.ToSnake(modelName)+"_service_impl.go")
+		case "service.go.tmpl":
+			path = gpath.Join(root, "/service/"+strcase.ToSnake(modelName)+"_service.go")
+		case "mapper.go.internal.tmpl":
+			path = gpath.Join(root, "/mapper/internal/"+strcase.ToSnake(modelName)+"_mapper_impl.go")
+		case "mapper.go.tmpl":
+			path = gpath.Join(root, "/mapper/"+strcase.ToSnake(modelName)+"_mapper.go")
+		case "model.go.tmpl":
+			path = gpath.Join(root, "/model/"+strcase.ToSnake(modelName)+".go")
+		}
+
+		gpath.MkParentDir(path)
+		fp, _ := os.Create(path)
+
+		err = tmpl.Execute(fp, data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
