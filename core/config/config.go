6548d8e60120fe837569068a99823dcbce393af5
khaosles <khaosles@163.com>
2023-07-01 10:07:04 +0800

    1

--- core/config/config.go
+++ core/config/config.go
@@ -8,13 +8,14 @@ package config
 */
 
 type Config struct {
-	System  System  `mapstructure:"system" json:"system" yaml:"system"`
-	Server  Server  `mapstructure:"server" json:"server" yaml:"server"`
-	Pgsql   Pgsql   `mapstructure:"pgsql" json:"pgsql" yaml:"pgsql"`
-	Mysql   Mysql   `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
-	Sqlite  Sqlite  `mapstructure:"sqlite" json:"sqlite" yaml:"sqlite"`
-	Logging Logging `mapstructure:"logging" json:"logging" yaml:"logging"`
-	Redis   Redis   `mapstructure:"redis" json:"redis" yaml:"redis"`
-	Etcd    Etcd    `mapstructure:"etcd" json:"etcd" yaml:"etcd"`
-	Rpc     Rpc     `mapstructure:"rpc" json:"rpc" yaml:"rpc"`
+	System   System   `mapstructure:"system" json:"system" yaml:"system"`
+	Server   Server   `mapstructure:"server" json:"server" yaml:"server"`
+	Pgsql    Pgsql    `mapstructure:"pgsql" json:"pgsql" yaml:"pgsql"`
+	Mysql    Mysql    `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
+	Sqlite   Sqlite   `mapstructure:"sqlite" json:"sqlite" yaml:"sqlite"`
+	Logging  Logging  `mapstructure:"logging" json:"logging" yaml:"logging"`
+	Redis    Redis    `mapstructure:"redis" json:"redis" yaml:"redis"`
+	Etcd     Etcd     `mapstructure:"etcd" json:"etcd" yaml:"etcd"`
+	Rpc      Rpc      `mapstructure:"rpc" json:"rpc" yaml:"rpc"`
+	Rocketmq Rocketmq `mapstructure:"rocketmq" json:"rocketmq" yaml:"rocketmq"`
 }
