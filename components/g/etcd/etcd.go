cf393874d621d0cf620bbf87de8e1391c78dd470
khaosles <khaosles@163.com>
2023-07-07 13:16:50 +0800

    1

--- components/g/etcd/etcd.go
+++ components/g/etcd/etcd.go
@@ -50,16 +50,17 @@ func Register(addr, serverName string, ttl int64) error {
 	if err != nil {
 		return err
 	}
-	alive, err := Client.KeepAlive(context.TODO(), lease.ID)
+	//alive, err := Client.KeepAlive(context.TODO(), lease.ID)
+	_, err = Client.KeepAlive(context.TODO(), lease.ID)
 	if err != nil {
 		return err
 	}
-	go func() {
-		for {
-			<-alive
-			glog.Debug(addr + " keep alive")
-		}
-	}()
+	//go func() {
+	//	for {
+	//		<-alive
+	//		glog.Debug(addr + " keep alive")
+	//	}
+	//}()
 	return nil
 }
 
